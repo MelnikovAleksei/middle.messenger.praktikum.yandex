b6fe034eb02a95dac1eb6bfa6f4312d6795df24e
Melnikov Aleksei <meln.a.a@outlook.com>
2022-01-26 14:43:23 +0300

    feat: add eslint

--- src/utils/setAppHeightStyleProperty.js
+++
@@ -1,9 +0,0 @@
-function setAppHeightStyleProperty() {
-  const APP_HEIGHT_STYLE_PROPERTY_NAME = '--app-height';
-
-  document.documentElement.style.setProperty(APP_HEIGHT_STYLE_PROPERTY_NAME, `${window.innerHeight}px`);
-}
-
-window.addEventListener('resize', setAppHeightStyleProperty);
-
-setAppHeightStyleProperty();
